48e64a16e6be1422614a331c2a9cb61c47533a2f
Tanish Grover <tanigrover@gmail.com>
2018-06-14 22:58:26 +0530

    Added Profile Info Form

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom' ;
 
 import Dashboard from './dashboard/Dashboard' ;
 import Profile from "./profile/Profile";
+import ProfileInfoForm from './profile/ProfileInfoForm';
 
 const RouterComponent = () => {
     return (
         <div>
             <Route exact path='/' component={Dashboard}/>
             <Route exact path='/profile' component={Profile}/>
+            <Route exact path='/profile/info' component={ProfileInfoForm}/>
         </div>
     )
 };
