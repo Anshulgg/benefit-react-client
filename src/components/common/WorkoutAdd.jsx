664a574f764467f64cf01f284a7ac01edb3f7121
Manish sahani <manishsahani1351111@gmail.com>
2018-07-13 16:00:53 +0530

    Add workouts done

--- src/components/common/WorkoutAdd.jsx
+++ src/components/common/WorkoutAdd.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Card, Row, Col, Form, Input, Button, AutoComplete, Select, Table, InputNumber, Popconfirm} from 'antd' ;
+import { Card, Row, Col, Form, Input, Button, AutoComplete, Select, Table, InputNumber, Popconfirm, message} from 'antd' ;
+import { addWorkout, addUserWorkout } from "../../actions/workoutAction";
 import {searchExercises} from "../../actions/exerciseAction";
 import EditableTable from './EditableTable';
 import uuid from 'uuid/v1' ;
-
+import serverConfig from '../../actions/server.config';
 
 const FormItem = Form.Item;
 const OptionAuto = AutoComplete.Option;
@@ -21,43 +22,83 @@ let columns = [
     {
         title: 'name',
         dataIndex: 'name',
-        width: '30%',
+        width: '25%',
         editable: false,
         inputType : 'text'
     },
     {
         title: 'reps',
         dataIndex: 'reps',
-        width: '25%',
+        width: '20%',
         editable: true,
         inputType : 'number'
     },
     {
         title: 'rest',
         dataIndex: 'rest',
-        width: '25%',
+        width: '20%',
         editable: true,
         inputType : 'number'
+    },
+    {
+        title: 'sets',
+        dataIndex: 'sets',
+        width: '20%',
+        editable: true,
+        inputType: 'number'
     }
 ]
 
 class WorkoutAdd extends Component {
-
-    state = {
-        exercises: [],
-        source: [],
-        selectedExercises: [],
-        exerciseValue : ''
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            exercises: [],
+            source: [],
+            selectedExercises: [],
+            exerciseValue: '',
+            date: '',
+            workout_id: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
 
     handleSubmit = (e) => {
         e.preventDefault();
+        const data = {
+            "client": this.props.clientId,
+            "date": this.props.date,
+            "workout": this.state.workout_id
+        }
+        if (this.state.workout_id !== '') {
+            addUserWorkout(data)
+            message.success('This is a message of success');           
+        }
+        else {
+            message.error('This is a message of error');
+        }
+
+                
+    };
+
+    handleSave = (e) => {
+        e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
+                const data = {
+                    "name": values.workoutName,
+                    "search_name": values.workoutSearchName,
+                    "description": values.workoutDescription,
+                    "exercises": this.state.selectedExercises
+                }
+                addWorkout(data).then(res => {                    
+                    this.setState({
+                        workout_id:  res
+                    }) 
+                })   
             }
         });
-    };
+    }
 
     handleExerciseSearch = (input) => {
         searchExercises(input).then(exercises => {
@@ -81,6 +122,7 @@ class WorkoutAdd extends Component {
         exercise = {
             reps : 10 ,
             rest : 10 ,
+            sets : 1,
             name : exercise.name,
             exercise : exercise._id ,
             key : uuid()
@@ -92,6 +134,9 @@ class WorkoutAdd extends Component {
         console.log("Value : " , this.props.form.getFieldValue('selectExercise'));
         this.props.form.setFieldsValue({'selectExercise' : ''})
         console.log("Value : " , this.props.form.getFieldValue('selectExercise'));
+        console.log(this.state.exercises);
+        console.log(this.state.selectedExercises);
+        
 
     };
 
@@ -112,18 +157,27 @@ class WorkoutAdd extends Component {
         const {getFieldDecorator} = this.props.form;
         return (
             <Card className="my-4">
-                <Form layout="inline" onSubmit={this.handleSubmit}>
+                <Form layout="inline">
                     <Row>
                         <Col span={8}>
-                            <h4>20-07-2017</h4>
+                            <h4>{this.props.date}</h4>
                         </Col>
                         <Col span={8} offset={8}>
                             <div className="float-right">
                                 <FormItem>
                                     <Button
-                                        style={{marginRight: '1.2rem'}}
+                                        style={{ marginRight: '1.2rem' }}
                                         type="primary"
                                         htmlType="submit"
+                                        onClick={this.handleSave}
+                                    >
+                                        Save
+                                    </Button>
+                                    <Button
+                                        style={{ marginRight: '1.2rem' }}
+                                        type="primary"
+                                        htmlType="submit"
+                                        onClick={this.handleSubmit}
                                     >
                                         Submit
                                     </Button>
@@ -135,6 +189,7 @@ class WorkoutAdd extends Component {
                                     </Button>
                                 </FormItem>
                             </div>
+
                         </Col>
                     </Row>
 
@@ -145,15 +200,16 @@ class WorkoutAdd extends Component {
                             <FormItem
                                 label="Workout name(visible to user)"
                                 colon={false}
-                                wrapperCol={{span: 24}}
+                                wrapperCol={{ span: 24 }}
                             >
                                 {getFieldDecorator('workoutName', {
-                                    rules: [{required: true, message: 'Please input workout name!'}],
+                                    rules: [{ required: true, message: 'Please input workout name!' }],
                                 })(
-                                    <Input placeholder="Workout Name"/>
+                                    <Input placeholder="Workout Name" />
                                 )}
                             </FormItem>
                         </Col>
+                        
 
                         <Col span={8}>
                             <FormItem
